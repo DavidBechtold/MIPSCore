27c9bae0b5afe64747a25764932e1f619db37a8d
David Bechtold <david.grim@gmx.at>
2015-08-03 12:23:21 +0200

    Added code to the gui

--- MIPSCoreUI/MainWindow.xaml.cs
+++ MIPSCoreUI/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MIPSCore;
+using MIPSCore.InstructionSet;
 
 using MIPSCore.ControlUnit;
 
@@ -25,18 +26,15 @@ namespace MIPSCoreUI
     /// 
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private CCore core;
+        private CCoreUI coreUI;
         public event PropertyChangedEventHandler PropertyChanged;
-        private CCoreUI coreUI;        
-       
+                
         public MainWindow()
         {
             InitializeComponent();
-            core = new CCore();
             coreUI = new CCoreUI();
             this.DataContext = coreUI;
         }
-
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -58,13 +56,37 @@ namespace MIPSCoreUI
             string test = "";
         }
 
+        private void ButtonClock_Click(object sender, RoutedEventArgs e)
+        {
+            coreUI.clock();
+        }
+
     }
 
     public class CCoreUI: INotifyPropertyChanged
     {
+        private CCore core;
+        private BackgroundWorker worker;
+        private enum Events{clocked, exception, completed};
+       
+        /* colors */
+        private SolidColorBrush instructionMemoryActive;
+        private SolidColorBrush registerFileActive;
+        private SolidColorBrush aluActive;
+        private SolidColorBrush dataMemoryActive;
+
+        /* control line colors */
+        private SolidColorBrush controlLineInactive = new SolidColorBrush(Colors.LightBlue);
+        private SolidColorBrush controlLineActive = new SolidColorBrush(Colors.Blue);
+        private SolidColorBrush branchControlLine;
+        private SolidColorBrush jumpControlLine;
+        private SolidColorBrush aluOperationControlLine;    
+        private SolidColorBrush regFileWriteControlLine;
+        private SolidColorBrush dataMemoryControlLine;
+        private SolidColorBrush aluSourceControlLine;
+        private SolidColorBrush regFileInputControlLine;
+
         public event PropertyChangedEventHandler PropertyChanged;
-        private CControlUnit control;
-        private SolidColorBrush registerWrite;
 
         private double adderWidth = 30;
         private double adderHeight = 30;
@@ -74,32 +96,116 @@ namespace MIPSCoreUI
 
         public CCoreUI()
         {
-            registerWrite = new SolidColorBrush(Colors.Black);
+            core = new CCore();
+            core.setMode(ExecutionMode.singleStep);
+            core.clocked += new EventHandler(clockedEvent);
+            core.completed += new EventHandler(completedEvent);
+            core.exception += new EventHandler(exceptionEvent);
+
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork +=new DoWorkEventHandler(worker_doWork);
+            worker.RunWorkerCompleted +=new RunWorkerCompletedEventHandler(worker_runWorkerCompleted);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_progressChanged);
+
+            branchControlLine = jumpControlLine = aluOperationControlLine = dataMemoryControlLine = aluSourceControlLine = regFileWriteControlLine = regFileInputControlLine = controlLineInactive;
+
+
+            core.programObjdump("Testcode//bubblesort.objdump");
+            core.startCore();
         }
 
-        public string Control
+        public void clock()
         {
-            set
-            {
-                string test = value;
-            }
-            get
-            {
-                return "TEST";
-            }
+            core.singleClock();
         }
 
-        public SolidColorBrush RegisterWrite
+        private void clockedEvent(Object obj, EventArgs args)
         {
-            set
+            /* let the work do from an background worker because the clocked event is not the wpf gui thread */
+            if (worker.IsBusy != true)
             {
-                registerWrite = value;
-                OnPropertyChanged("RegisterWrite");
+                worker.RunWorkerAsync();
             }
-            get
+            
+
+
+        }
+
+        private void completedEvent(Object obj, EventArgs args)
+        {
+        }
+
+        private void exceptionEvent(Object obj, EventArgs args)
+        {
+        }
+
+        private void worker_doWork(Object obj, EventArgs args)
+        {
+            /* Control Lines */
+            if (core.getControlUnit.getAluControl == ALUControl.stall)
+                this.ALUOperationControlLine = controlLineInactive;
+            else
             {
-                return registerWrite;
+                this.aluOperationControlLine = new SolidColorBrush(Colors.Blue);
+                OnPropertyChanged("ALUOperationControlLine");
             }
+
+            if (core.getControlUnit.getMemRead == true || core.getControlUnit.getMemWrite == true)
+                this.DataMemoryControlLine = controlLineActive;
+            else
+                this.DataMemoryControlLine = controlLineInactive;
+        }
+
+        private void worker_runWorkerCompleted(Object obj, EventArgs args)
+        {
+        }
+
+        private void worker_progressChanged(Object obj, EventArgs args)
+        {
+        }
+
+        public SolidColorBrush BranchControlLine
+        {
+            set { branchControlLine = value; OnPropertyChanged("BranchControlLine"); }
+            get { return branchControlLine; }
+        }
+
+        public SolidColorBrush JumpControlLine
+        {
+            set { jumpControlLine = value; OnPropertyChanged("JumpControlLine"); }
+            get { return jumpControlLine; }
+        }
+
+        public SolidColorBrush ALUSourceControlLine
+        {
+            set { aluSourceControlLine = value; OnPropertyChanged("ALUSourceControlLine"); }
+            get { return aluSourceControlLine; }
+        }
+
+        public SolidColorBrush ALUOperationControlLine
+        {
+            set { aluSourceControlLine = value; OnPropertyChanged("ALUOperationControlLine"); }
+            get { return aluOperationControlLine; }
+        }
+
+        public SolidColorBrush DataMemoryControlLine
+        {
+            set { dataMemoryControlLine = value; OnPropertyChanged("DataMemoryControlLine"); }
+            get { return dataMemoryControlLine; }
+        }
+
+        public SolidColorBrush RegFileInputControlLine
+        {
+            set { regFileInputControlLine = value; OnPropertyChanged("RegFileInputControlLine"); }
+            get { return regFileInputControlLine; }
+        }
+
+        public SolidColorBrush RegFileWriteControlLine
+        {
+            set { regFileWriteControlLine = value; OnPropertyChanged("RegFileWriteControlLine"); }
+            get { return regFileWriteControlLine; }
         }
 
         public double RectangleWidth
